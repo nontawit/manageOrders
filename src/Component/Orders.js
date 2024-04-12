87931fa560417c4d0d73e824e1f4beba836a11b2
nontawit <ntwitmn@gmail.com>
2024-04-13 01:57:53 +0700

    new

---
+++ src/Component/Orders.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import axios from "axios";
+
+function Orders(){
+    return(
+        <div>
+            <h1>Show orders</h1>
+        </div>
+    )
+}
+
+export default Orders;
