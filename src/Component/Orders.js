c08ed3f7e11042c411c4da638c7ca03eb1c37e5d
nontawit <ntwitmn@gmail.com>
2024-04-13 02:14:01 +0700

    ;

--- src/Component/Orders.js
+++ src/Component/Orders.js
@@ -1,10 +1,30 @@
-import React from "react";
-// import axios from "axios";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Orders(){
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://restapi-tjap.onrender.com/api/orders')
+        .then(response => {
+            setOrders(response.data);
+            console.log(setOrders);
+        })
+        .catch(error => {
+            console.log('Error fetching order: ', error);
+        });
+    }, []);
+
     return(
         <div>
             <h1>Show orders</h1>
+            <ul>
+                {orders.map(order => (
+                    <li key={order._id}>
+                        Name: {order.cusName}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
