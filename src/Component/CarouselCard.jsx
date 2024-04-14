ee441bae6a0cbbd3d4658813d01f09e7347e8ead
nontawit <ntwitmn@gmail.com>
2024-04-15 01:32:27 +0700

    Update CarouselCard.jsx

--- src/Component/CarouselCard.jsx
+++ src/Component/CarouselCard.jsx
@@ -1,18 +1,19 @@
-import {Carousel} from "react-bootstrap"
+import {Carousel} from "react-bootstrap";
+import Card from 'react-bootstrap/Card';
 
 function CarouselCard({ orders }){
     return(
         <Carousel>
             {orders.map(order => (
                 <Carousel.Item key={order._id}>
-                    <div>
+                    <Card style={{ width: '18rem', margin: "auto" }}>
                         <h3>ลูกค้า: {order.cusName}</h3>
                         <p>ที่อยู่: {order.cusAddress}</p>
                         <p>{order.orderUnit} ชุด</p>
                         <p> ส่ง: {order.dateDelivery}</p>
                         <p>สถานะ: {order.orderStatus}</p>    
                         {order.orderDate}
-                    </div>
+                    </Card>
                 </Carousel.Item>
             ))}
         </Carousel>
