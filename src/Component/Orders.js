45f4948a58b5496a60d5194d8b8834a2f3beb45d
nontawit <ntwitmn@gmail.com>
2024-04-28 00:41:50 +0700

    btnConfig

--- src/Component/Orders.js
+++ src/Component/Orders.js
@@ -33,9 +33,16 @@ function Orders() {
 
   if (loading) {
     return (
-      <div className="conLoader">
-        <div className="txtLoader">กำลังประมวลผล.....</div>
-        <div className="loader"></div>
+      <div class="loader">
+        <span class="l">L</span>
+        <span class="o">o</span>
+        <span class="a">a</span>
+        <span class="d">d</span>
+        <span class="i">i</span>
+        <span class="n">n</span>
+        <span class="g">g</span>
+        <span class="d1">.</span>
+        <span class="d2">.</span>
       </div>
     );
   }
@@ -56,6 +63,7 @@ function Orders() {
     Swal.fire({
       title: `ลูกค้า "${order.cusName}"`,
       text: 'จัดการออเดอร์นี้อย่างไร ?',
+      icon: "question",
       showDenyButton: true,
       showCancelButton: true,
       confirmButtonText: "เสร็จสิ้น",
@@ -78,13 +86,6 @@ function Orders() {
     }
   };
 
-
-
-
-  // const handleClosePopup = () => {
-  //   setShowPopupConfig(false);
-  //   setSelectedOrder(null);
-  // };
 
   const pendingOrders = orders.filter((order) => order.orderStatus === "รอดำเนินการ");
 
@@ -135,15 +136,23 @@ function Orders() {
                   </div>
                 </Card.Text>
               </Card.Body>
-              <div className="conBtnConfig">
+              {/* <div className="conBtnConfig">
                 <Button
                   variant="outlined"
                   className="btnConfig"
                   onClick={() => popupConfig(order)}
                 >
-                <span><SettingsOutlinedIcon sx={{ fontSize: 30 }}/></span>
-                  จัดการ
+               <SettingsOutlinedIcon sx={{ fontSize: 30 }}/>
                 </Button>
+              </div> */}
+              <div className="conBtnConfig">
+                <button title="filter" class="filter" onClick={() => popupConfig(order)}>
+                  <svg viewBox="0 0 512 512" height="1em">
+                    <path
+                      d="M0 416c0 17.7 14.3 32 32 32l54.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 448c17.7 0 32-14.3 32-32s-14.3-32-32-32l-246.7 0c-12.3-28.3-40.5-48-73.3-48s-61 19.7-73.3 48L32 384c-17.7 0-32 14.3-32 32zm128 0a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM320 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm32-80c-32.8 0-61 19.7-73.3 48L32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l246.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48l54.7 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-54.7 0c-12.3-28.3-40.5-48-73.3-48zM192 128a32 32 0 1 1 0-64 32 32 0 1 1 0 64zm73.3-64C253 35.7 224.8 16 192 16s-61 19.7-73.3 48L32 64C14.3 64 0 78.3 0 96s14.3 32 32 32l86.7 0c12.3 28.3 40.5 48 73.3 48s61-19.7 73.3-48L480 128c17.7 0 32-14.3 32-32s-14.3-32-32-32L265.3 64z"
+                    ></path>
+                  </svg>
+                </button>
               </div>
             </Card>
           ))
